9da4e1cb4c5755531454b47cf09733ee693815bc
AsafMainPC <gorff55@gmail.com>
2016-07-01 17:21:51 +0300

    run simple angular 2 application for demo purpose

--- demo/app.component.ts
+++ demo/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>hello dima</h1>'
 })
+
 export class AppComponent { }
